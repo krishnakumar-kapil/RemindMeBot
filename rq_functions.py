6b12ea77e7bb8ae05a429e68ce4c434fdb6b7c16
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2017-02-13 22:55:54 -0600

    changed timeout

--- rq_functions.py
+++ rq_functions.py
@@ -4,12 +4,11 @@ from time import sleep
 import messenger
 
 def add_to_queue(sender_id, reminder_str, delta_time):
-    job = q.enqueue(send_reminder_worker, sender_id, reminder_str, delta_time, timeout=86400)
+    job = q.enqueue(send_reminder_worker, sender_id, reminder_str, delta_time)
 
 def send_reminder_worker(sender_id, reminder_str, delta_time):
     sleep(float(delta_time))
-    print("HELLOOOOOO")
     messenger.fb_message(sender_id, reminder_str)
 
 redis_conn = Redis()
-q = Queue(connection=redis_conn)
+q = Queue(connection=redis_conn, default_timeout=86400)
